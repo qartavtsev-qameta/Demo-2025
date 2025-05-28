c51c9bd60707cb69dd01a650ae4ccaa7406297fc
Alexander QA <112622680+qartavtsev-qameta@users.noreply.github.com>
2025-05-28 22:03:07 +0300

    Create examples.py

---
+++ examples.py
@@ -0,0 +1,18 @@
+import pytest
+import random
+import time
+import allure
+
+@allure.title("Успешный тест с ожиданием")
+def test_success_with_random_delay():
+    delay = random.randint(1, 30)
+    with allure.step(f"Ожидание {delay} секунд"):
+        time.sleep(delay)
+    assert True, "Тест завершился успешно"
+
+@allure.title("Неуспешный тест с ожиданием")
+def test_failure_with_random_delay():
+    delay = random.randint(1, 30)
+    with allure.step(f"Ожидание {delay} секунд"):
+        time.sleep(delay)
+    assert False, "Тест завершился с ошибкой"
